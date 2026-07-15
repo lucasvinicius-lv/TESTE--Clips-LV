21f7c329020a2cc21ea0614fdf4c433da31c15f4
lucasvinicius-lv <lucasgtavinicius@gmail.com>
2026-07-15 10:29:01 -0300

    Update app.py

--- clips-ai-main/app.py
+++ clips-ai-main/app.py
@@ -202,6 +202,8 @@ def run_download(job_id, url, format_type):
     cmd += ['--extractor-args', 'youtube:player_client=web']
     cmd += ['--extractor-args', f'youtubepot-bgutilhttp:base_url={BGUTIL_POT_BASE_URL}']
     #cmd += ['--js-runtimes', 'node']
+    cmd += ['--plugin-embeds', 'bgutil-ytdlp-pot-provider']
+    cmd += ['--js-runtimes', 'bgutil-ytdlp-pot-provider']
 
     if os.path.isfile(COOKIES_PATH):
         cmd += ['--cookies', COOKIES_PATH]
