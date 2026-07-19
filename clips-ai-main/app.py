f41c5c830eb84adfd2bf29228d29cc814550ee23
lucasvinicius-lv <lucasgtavinicius@gmail.com>
2026-07-19 17:06:43 -0300

    Update app.py

--- clips-ai-main/app.py
+++ clips-ai-main/app.py
@@ -14,6 +14,7 @@ import json
 
 from flask import Flask, request, jsonify, send_file, Response
 from flask_cors import CORS
+from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
